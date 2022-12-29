fcfa1554f94078c4c742516b4d3259378b3d8703
Effi-S <effi.szt@gmail.com>
2022-12-29 15:48:29 +0200

    Created GithubHandler utility

---
+++ git_utils.py
@@ -0,0 +1,44 @@
+import argparse
+import requests
+
+
+class GithubHandler:
+    """ Handler for reading info and running actions on a Github Repo"""
+
+    def __init__(self, repo: str, token: str):
+        self.client = requests.Session()
+        self.client.auth = (token, '')
+        self.repo = f'https://api.github.com/repos/{repo}'
+        print('Connecting to Repo:', repo)
+
+    def iter_github_files(self, path: str = 'contents') -> dict:
+        """Given a URL of an endpoint in a Github Repo and a path recursively generates the file items """
+        response = self.client.get(url := f'{self.repo}/{path}')
+        assert response.status_code == 200, f'Error getting files from URL: {url} ({response.status_code})'
+
+        for item in response.json():
+            if type_ := item['type'] == 'file':
+                print('File:', item["path"])
+                yield item
+            elif type_ == 'dir':
+                print('Directory:', item["path"])
+                # List the contents of the directory recursively
+                for x in self.iter_github_files(item['path']):
+                    yield x
+            else:
+                print(f'Unknown item type: {type_} ({item["path"]})')
+
+    def read_file_url(self, item: str | dict) -> str:
+        """Given a URL or Json Item for a Github file, reads the content """
+        if isinstance(item, dict):
+            item = item['download_url']
+        file_response = self.client.get(item)
+        assert file_response.status_code == 200, f'Error Reading file in URL: {response.status_code}'
+        return file_response.text
+
+    def create_issue(self, title: str, body: str = 'Issue Body'):
+        url = f'{repo}/issues'
+        payload = {'title': title, 'body': body}
+        response = self.client.post(url, json=payload)
+        assert response.status_code == 201, f'Error creating issue for {file}: {response.status_code}'
+        print(f'Issue created: "{title}"')
--- main.py
+++ main.py
@@ -1,47 +1,30 @@
 """POC running a program that creates an Issue in our repo for each .py file.
 Example Usage:
-python main.py --user ${{ github.actor }} --repo ${{ github.repository }} --token ${{ secrets.GITHUB_TOKEN }}
+python main.py  --repo ${{ github.repository }} --token ${{ secrets.GITHUB_TOKEN }}
 """
-import argparse
-import requests
+import git_utils
 
 
 def main():
     """POC running a program that creates Issues in another Repo"""
     # Parse command-line arguments
     parser = argparse.ArgumentParser(description=__doc__)
-    parser.add_argument('--user', required=True, help="Pass here: ${{ github.actor }} ")
     parser.add_argument('--repo', required=True, help="Pass here: ${{ github.repository}}")
     parser.add_argument('--token', required=True, help="Pass here: ${{ secrets.GITHUB_TOKEN }}")
     args = parser.parse_args()
 
-    print(args.user, args.repo, args.token)
-    args.repo = '/'.join(x for x in args.repo.split('/') if x != args.user)  # remove user from path
     # Set up the API client
-    client = requests.Session()
-    client.auth = (args.token, '')
+    github_handler = git_utils.GithubHandler(args.repo, args.token)
 
-    # List the contents of the repository
-    print(url := f'https://api.github.com/repos/{args.user}/{args.repo}/contents')
-    response = client.get(url)
-
-    assert response.status_code == 200, f'Error getting files: {response.status_code}'
-
-    # Loop through the list of files
-    print(json_ := response.json())
-    for item in json_:
-        if item['name'].endswith('.py'):
-            # Create an Issue for each .py file
-            url = f'https://api.github.com/repos/{args.user}/{args.repo}/issues'
-            payload = {
-                'title': item['name'],
-                'body': 'This is the body of the issue.'
-            }
-            response = client.post(url, json=payload)
-            if response.status_code == 201:
-                print(f'Issue created for {item["name"]}')
-            else:
-                print(f'Error creating issue for {item["name"]}: {response.status_code}')
+    # Loop through the files of the repository
+    for file_item in github_handler.iter_github_files():
+        if file_item['name'].endswith('.py'):
+            # Read the python file
+            text = github_handler.read_file_url(file_item)
+            # Create the Issue body
+            dummy_body = ''.join(text.splitlines()[-1:])  # Just the last line of the file, for POC
+            # Create an Issue
+            github_handler.create_issue(title=file_item['name'], body=dummy_body)
 
 
 if __name__ == '__main__':
